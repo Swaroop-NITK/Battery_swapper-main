e8e83ae84eafbe73fdb4fac6e1fe8ceea5d70681
Swaroop-NITK <swaroopvoonna.2002@gmail.com>
2022-05-04 14:51:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,250 @@
+from click import pass_context
+from flask import Flask,json,redirect, render_template,flash,request
+from flask.globals import request,session
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import VARCHAR
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_required,logout_user,login_user,login_manager,LoginManager,current_user
+
+from flask_mail import Mail
+import json
+
+# mydatabase connecction
+local_server=True
+app=Flask(__name__) 
+app.secret_key="secretkey"
+
+with open('config.json','r') as c:
+    params=json.load(c)["params"]
+
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
+)
+mail = Mail(app)
+
+# this is for getting the unique user access
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return customer.query.get(int(user_id)) or dealer.query.get(int(user_id))
+
+# app.config['SQLALCHEMY_DATABASE_URI']='mysql://username:password@localhost/databasename'3306
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost:3307/battery_swap'
+db=SQLAlchemy(app)
+
+#creating db models
+class Test(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50))
+ 
+class customer(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String,unique=True)
+    name=db.Column(db.String(40))
+    dob=db.Column(db.String(1000))
+    phone_number=db.Column(db.String(10),unique=True)
+
+class dealer(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    centre_id=db.Column(db.String(20),unique=True)
+    email=db.Column(db.String(100))
+    password=db.Column(db.String(1000))
+
+class centre(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    centre_id=db.Column(db.String(20),unique=True)
+    centre_name=db.Column(db.String(50))
+    no_of_batteries=db.Column(db.Integer)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/usersignup")
+def usersignup():
+    return render_template("usersignup.html")
+
+
+@app.route("/alllogin")
+def alllogin():
+    return render_template("alllogin.html")
+
+@app.route("/userlogin")
+def userlogin():
+    return render_template("userlogin.html")
+
+@app.route('/signup',methods=['POST','GET'])
+def signup():
+    if request.method=="POST":
+        email=request.form.get('email')
+        name=request.form.get('name')
+        dob=request.form.get('dob')
+        phone_number=request.form.get('phone-number')
+        # print(email,name,dob,phone_number)
+        encpassword=generate_password_hash(dob)
+        user=customer.query.filter_by(email=email).first()
+        phone=customer.query.filter_by(phone_number=phone_number).first()
+        if user or phone:
+            flash("Email or phone number is already taken","warning")
+            return render_template("usersignup.html")
+        new_user=db.engine.execute(f"INSERT INTO `customer`(`email`,`name`,`dob`,`phone_number`) VALUES ('{email}','{name}','{encpassword}','{phone_number}')")
+    
+        flash("Signup Success, PLEASE LOGIN","success")
+        return render_template("userlogin.html") 
+
+    return render_template("usersignup.html")
+
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=="POST":
+        email=request.form.get('email')
+        dob=request.form.get('dob')
+        user=customer.query.filter_by(email=email).first()
+        if user and check_password_hash(user.dob,dob):
+            login_user(user)
+            flash("Login Success","info")
+            return render_template("index.html")
+        else:
+            flash("Invalid Credentials","danger")
+            return render_template("userlogin.html")
+
+
+    return render_template("userlogin.html")
+
+@app.route('/dealerlogin',methods=['POST','GET'])
+def dealerlogin():
+    if request.method=="POST":
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=dealer.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password,password):
+            login_user(user)
+            flash("Login Success","info")
+            return render_template("index.html")
+        else:
+            flash("Invalid Credentials","danger")
+            return render_template("dealerlogin.html")
+
+
+    return render_template("dealerlogin.html")    
+
+@app.route('/admin',methods=['POST','GET'])
+def admin():
+    if request.method=="POST":
+        username=request.form.get('username')
+        password=request.form.get('password')
+        if(username==params['user'] and password==params['password']):
+            session['user']=username
+            flash("login success","info")
+            return render_template("adddealer.html")
+        else:
+            flash("Invalid credentials","danger")    
+            
+    return render_template("admin.html")
+  
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logout SuccessFul","warning")
+    return redirect(url_for('login'))
+
+#@app.route("/dealerlogin")
+#def dealerlogin():
+#    return render_template("dealerlogin.html")
+
+
+@app.route("/aboutus")
+def aboutus():
+    return render_template("aboutus.html") 
+
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.html")
+
+@app.route('/adddealer',methods=['POST','GET'])
+def adddealer():
+
+    if('user' in session and session['user']==params['user']):
+       
+      if request.method=="POST":
+        centre_id=request.form.get('centre_id')
+        email=request.form.get('email')
+        password=request.form.get('password')
+        encpassword=generate_password_hash(password)
+        centre_id=centre_id.upper()
+        emaildealer=dealer.query.filter_by(email=email).first()
+        if emaildealer:
+            flash("Email is already taken","warning")
+            return render_template("adddealer.html")
+
+        db.engine.execute(f"INSERT INTO `dealer`(`centre_id`,`email`,`password`) VALUES ('{centre_id}','{email}','{encpassword}')")
+        
+        mail.send_message('BATTERY SWAP',sender=params['gmail-user'],recipients=[email],body=f"Welcome, Thanks for contributing to the future\nYour Login credentials Are:\nEmail Address: {email}\nPassword: {password}\nCentre ID:{centre_id}\n\nDo not share your password\n\n\nThank you")
+
+        flash("Dealer Added Successfully","info")
+        return render_template("adddealer.html")
+
+    else:
+      flash("Login and try again", "warning")
+      return render_template("adddealer.html")
+    
+
+# testing whether db is connected or not
+@app.route("/test")
+def test():
+    try:
+        a=customer.query.all()
+        print(a)
+        return f'MY DATABASE IS CONNECTED {a} '
+    except Exception as e:
+            print(e)
+            return f'MY DATABASE IS NOT CONNECTED {e}'
+
+@app.route("/logoutadmin")
+def logoutadmin():
+    session.pop('user')
+    flash("You are logged out", "primary")
+    return redirect('/admin')
+
+@app.route("/addcentredata",methods=['POST','GET'])
+def addcentredata():
+
+    email=current_user.email
+    posts=dealer.query.filter_by(email=email).first()
+    code=posts.centre_id
+    postsdata=centre.query.filter_by(centre_id=code).first()
+
+    if request.method=="POST":
+        centre_id=request.form.get('centre_id')
+        centre_name=request.form.get('centre_name')
+        no_of_batteries=request.form.get('no_of_batteries')
+        centre_id=centre_id.upper()
+        dealer_1=dealer.query.filter_by(centre_id=centre_id).first()
+        centre_1=centre.query.filter_by(centre_id=centre_id).first()
+        if centre_1:
+            flash("Centre is added, You can update it","primary")
+            return render_template("centredata.html")
+        if dealer_1:
+            db.engine.execute(f"INSERT INTO `centre` (`centre_id`,`centre_name`,`no_of_batteries`) VALUES('{centre_id}','{centre_name}','{no_of_batteries}')")
+            flash("Centre Added","primary")
+        else:
+            flash("Centre ID doesnt exist","warning")
+
+    #return render_template("centredata.html",postsdata=postsdata)
+    return render_template("centredata.html",postsdata=postsdata)
+
+
+
+app.run(debug=True) 
